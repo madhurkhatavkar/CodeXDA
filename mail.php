23b391d9c8759af8c67603bdc12edafca7400434
Madhur Khatavkar <77504674+madhurkhatavkar@users.noreply.github.com>
2021-08-30 12:56:31 +0530

    Update mail.php

--- mail.php
+++ mail.php
@@ -1,15 +1,14 @@
 <?php
-if(isset($_POST['submit'])){  
+if (isset($_POST['submit'])){  
 $email= $_POST['email'];
 $message= $_POST['message'];
 $to = "madhurdk2001@gmail.com";
 $subject = "Mail From CodeXDA";
-$txt ="Email = " . $email . "\r\n Message =" . $message;
+$txt ="Email = " . $email . "\n\n Message =" . $message;
 $headers = "From: noreply@codexda.com";
-if($email!=NULL){
-    mail($to,$subject,$txt,$headers);
-}
+
+mail($to,$subject,$txt,$headers);
 //redirect
-header("Location: Contact.html");
+header("Location: mail.php?mailsent");
 }
 ?>
