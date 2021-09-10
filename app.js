944a45ae46c264a42ee160ee5da8abf19d25b58d
Madhur Khatavkar <77504674+madhurkhatavkar@users.noreply.github.com>
2021-09-10 16:44:15 +0530

    Update app.js

--- app.js
+++ app.js
@@ -9,3 +9,15 @@ if ('serviceWorker' in navigator) {
     });
   });
 }
+
+window.addEventListener('beforeinstallprompt', e => {
+  if (window.matchMedia('(display-mode: standalone)').matches) {
+      // don't display install banner when installed
+      return e.preventDefault();
+  } else {
+      const btn = document.querySelector('#install')
+      btn.hidden = false;
+      btn.onclick = _ => e.prompt();
+      return e.preventDefault();
+  }
+});
